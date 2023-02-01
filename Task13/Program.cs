40cf82bb7ea31bb7cf9f4afc03eac94524b6722c
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-01 15:52:30 +1000

    Task13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Random rnd = new Random();
+int number = rnd.Next();
+System.Console.WriteLine(number);
+
+if(number <100)
+{
+    System.Console.WriteLine("There is no 3rd digit");
+}
+
+if(number >= 100)
+{
+    while(number > 999)
+{
+    number = number/10;
+}
+}
+int result = number%10;
+
+System.Console.WriteLine(result);
