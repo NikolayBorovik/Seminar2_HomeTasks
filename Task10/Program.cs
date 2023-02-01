b43afb47d91e63d0c39a5c7bd0d7a10e1f999b85
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-01 15:29:17 +1000

    Task10 done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру 
+// этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+System.Console.WriteLine("Enter your 1st number: ");
+string? strNumber = Console.ReadLine();
+int numberN = int.Parse(strNumber);
+
+if(numberN < 100 || numberN > 999)
+{
+    System.Console.WriteLine("You have enetered a wrong number");
+}
+
+int number1  = numberN%100;
+int number2 = number1%10;
+int result = (number1-number2)/10;
+
+System.Console.WriteLine(result);
